d06d3a75c766e797b352d88c260b553669fb4820
JLiikanen <jakke.liikanen@gmail.com>
2021-12-04 16:30:44 +0200

    commit testaus - GUI lisätty!

--- testausmoduuli/Ohjelma/gui.scala
+++ testausmoduuli/Ohjelma/gui.scala
@@ -1,5 +1,18 @@
 package Ohjelma
 
-object gui {
+object gui extends App {
+
+  this.run()
+
+
+  private def run() = {
+    var i = 0
+    while (i < 5){
+    println("MOIKKA")
+      if (i == 4)
+        println("Viimeinen moikka!")
+      i += 1
+    }
+  }
 
 }
